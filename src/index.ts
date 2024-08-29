cad49b40fdaaeb15c7037d00829e3741ce40c802
Farhan Muzaki <farhan.muzaki@e2pay.co.id>
2024-08-29 15:10:54 +0700

    feat: add simple telebot handler

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,103 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import { Client } from './driver/mysql'
+
+// Bot Handler example from https://github.com/cvzi/telegram-bot-cloudflare/blob/main/bot.js
+const TOKEN = BOT_TOKEN
+const SECRET = BOT_SECRET
+const WEBHOOK = '/endpoint'
+
+/**
+ * Wait for requests to the worker
+ */
+addEventListener('fetch', event => {
+  const url = new URL(event.request.url)
+  if (url.pathname === WEBHOOK) {
+    event.respondWith(handleWebhook(event))
+  } else if (url.pathname === '/registerWebhook') {
+    event.respondWith(registerWebhook(event, url, WEBHOOK, SECRET))
+  } else if (url.pathname === '/unRegisterWebhook') {
+    event.respondWith(unRegisterWebhook(event))
+  } else {
+    event.respondWith(new Response('No handler for this request test'))
+  }
+})
+
+/**
+ * Handle requests to WEBHOOK
+ * https://core.telegram.org/bots/api#update
+ */
+async function handleWebhook (event) {
+  // Check secret
+  if (event.request.headers.get('X-Telegram-Bot-Api-Secret-Token') !== SECRET) {
+    return new Response('Unauthorized', { status: 403 })
+  }
+
+  // Read request body synchronously
+  const update = await event.request.json()
+  // Deal with response asynchronously
+  event.waitUntil(onUpdate(update))
+
+  return new Response('Ok')
+}
+
+/**
+ * Handle incoming Update
+ * https://core.telegram.org/bots/api#update
+ */
+async function onUpdate (update) {
+  if ('message' in update) {
+    await onMessage(update.message)
+  }
+}
+
+/**
+ * Handle incoming Message
+ * https://core.telegram.org/bots/api#message
+ */
+function onMessage (message) {
+  return sendPlainText(message.chat.id, 'Echo:\n' + message.text)
+}
+
+/**
+ * Send plain text message
+ * https://core.telegram.org/bots/api#sendmessage
+ */
+async function sendPlainText (chatId, text) {
+  return (await fetch(apiUrl('sendMessage', {
+    chat_id: chatId,
+    text
+  }))).json()
+}
+
+/**
+ * Set webhook to this worker's url
+ * https://core.telegram.org/bots/api#setwebhook
+ */
+async function registerWebhook (event, requestUrl, suffix, secret) {
+  // https://core.telegram.org/bots/api#setwebhook
+  const webhookUrl = `${requestUrl.protocol}//${requestUrl.hostname}${suffix}`
+  const r = await (await fetch(apiUrl('setWebhook', { url: webhookUrl, secret_token: secret }))).json()
+  return new Response('ok' in r && r.ok ? 'Ok' : JSON.stringify(r, null, 2))
+}
+
+/**
+ * Remove webhook
+ * https://core.telegram.org/bots/api#setwebhook
+ */
+async function unRegisterWebhook (event) {
+  const r = await (await fetch(apiUrl('setWebhook', { url: '' }))).json()
+  return new Response('ok' in r && r.ok ? 'Ok' : JSON.stringify(r, null, 2))
+}
+
+/**
+ * Return url to telegram api, optionally with parameters added
+ */
+function apiUrl (methodName, params = null) {
+  let query = ''
+  if (params) {
+    query = '?' + new URLSearchParams(params).toString()
+  }
+  return `https://api.telegram.org/bot${TOKEN}/${methodName}${query}`
+}
